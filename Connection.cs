0a08aaf2160d440c6864a04ec553cb94b63ddf2c
aidan818 <107783500+aidan818@users.noreply.github.com>
2022-06-19 00:19:15 -0500

    Add files via upload

---
+++ Connection.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Threading;
+//using AppKit.NSViewController;
+using Impinj.OctaneSdk;
+using Foundation;
+using AppKit;
+
+namespace impinjspeedwayreader
+{
+	public class Connection
+	{
+        public Connection()
+        { }
+            public static void ConnectToReader()
+            {
+                try
+                {
+                    Console.WriteLine("Attempting to connect to {0} ({1}).",
+                        GlobalVariable.reader.Name, GlobalVariable.Host_IP);
+
+                    // Number of milliseconds before a 
+                    // connection attempt times out.
+                    GlobalVariable.reader.ConnectTimeout = 6000;
+                // Connect to the reader.
+                // Change the ReaderHostname constant in SolutionConstants.cs 
+                // to the IP address or hostname of your reader.
+                    //const string testIP = "169.254.81.242";
+                    GlobalVariable.reader.Connect(GlobalVariable.Host_IP);
+                    Console.WriteLine("Successfully connected.");
+
+                    // Tell the reader to send us any tag reports and 
+                    // events we missed while we were disconnected.
+                    GlobalVariable.reader.ResumeEventsAndReports();
+                }
+                catch (OctaneSdkException e)
+                {
+                    Console.WriteLine("Failed to connect. Please try again");
+                    throw e;
+                }
+            }
+
+            public static void OnConnectionLost(ImpinjReader reader)
+            {
+                // This event handler is called if the reader  
+                // stops sending keepalive messages (connection lost).
+                Console.WriteLine("Connection lost : {0} ({1})", reader.Name, reader.Address);
+
+                // Cleanup
+                reader.Disconnect();
+
+                // Try reconnecting to the reader
+                ConnectToReader();
+            }
+
+            public static void OnKeepaliveReceived(ImpinjReader reader)
+            {
+                // This event handler is called when a keepalive 
+                // message is received from the reader.
+                Console.WriteLine("Keepalive received from {0} ({1})", reader.Name, reader.Address);
+            }
+
+        public static void OnTagsReported(ImpinjReader reader, TagReport report)
+            {
+            // This event handler is called asynchronously 
+            // when tag reports are available.
+            // Loop through each tag in the report 
+            // and print the data.
+                foreach (Tag tag in report)
+                {
+
+                Console.WriteLine("EPC : {0} Timestamp : {1}", tag.Epc, tag.LastSeenTime);
+                
+                GlobalVariable.EPC = tag.Epc.ToHexWordString();
+                GlobalVariable.LastTimeSeen = tag.LastSeenTime.ToString();
+               
+                Console.WriteLine("EPC: {0}. Timestamp: {1}", GlobalVariable.EPC, GlobalVariable.LastTimeSeen);
+                UpdateData(GlobalVariable.EPC, GlobalVariable.LastTimeSeen);
+                
+                }
+            }
+
+        
+        
+        public static void UpdateData(string EPC, string LastTimeSeen)
+        {
+            
+            //SAME ERRORS as the view controller file
+            ViewController.UpdateData(EPC, LastTimeSeen);
+            Console.WriteLine("UPDATED DATA");
+           
+        }
+
+     }
+}
+
---
+++ ViewController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Threading;
+
+using AppKit;
+using Foundation;
+
+using Impinj.OctaneSdk;
+
+
+namespace impinjspeedwayreader
+{
+	public partial class ViewController : NSViewController
+	{
+		public ViewController(IntPtr handle) : base(handle)
+		{
+		}
+
+        //public ViewController()
+        //{
+        //}
+
+        public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+
+			UpdateData(GlobalVariable.EPC, GlobalVariable.LastTimeSeen, ViewController);
+		}
+
+
+		//Function to transfer data onto the table
+		//Made it public to try to call it in my main for loop in connections.cs
+		// I cannot make this to a static or I will get an CS0120 Error
+		// to fix that I must add the ViewController View but this creates an ERROR when the function is called
+		// as I do not know what to put for the data for viewcontroller spot
+		public static void UpdateData(string EPC, string LastTimeSeen, ViewController view)
+        {
+			view.AddData(new ReaderData(EPC, LastTimeSeen));
+			view.ReaderTable.ReloadData();
+		}
+		
+
+		public override NSObject RepresentedObject {
+			get {
+				return base.RepresentedObject;
+			}
+			set {
+				base.RepresentedObject = value;
+				// Update the view, if already loaded.
+			}
+		}
+
+
+		partial void StartButtonAction(NSButton sender)
+        {
+			NewSettings.CustomSettings();
+			
+			//Console.WriteLine("EPC: {0}. Timestamp: {1}", GlobalVariable.EPC, GlobalVariable.LastTimeSeen);
+			//Call a method/function that will start taking the data and providing it to the table
+		}
+		partial void StopButtonAction(NSButton sender)
+        {
+			GlobalVariable.reader.Stop();
+			Console.WriteLine("Reader Stop");
+        }
+
+        partial void SubmitIPAction(NSButton sender)
+        {
+			GlobalVariable.Host_IP = EnterIPOutlet.StringValue;
+			GlobalVariable.reader.Name = EnterNameOutlet.StringValue;
+			Console.WriteLine("The reader is now named: " + GlobalVariable.reader.Name);
+			Console.WriteLine("Now attempting to connect to: " + GlobalVariable.Host_IP);
+			Connection.ConnectToReader();
+			//Call function to predetermine the settings if reader was connected successfully
+        }
+
+		
+		private NSMutableArray _data = new NSMutableArray();
+
+		[Export("ReaderDataArray")]
+		public NSArray Data
+		{
+			get { return _data; }
+		}
+
+		[Export("addObject:")]
+		public void AddData(ReaderData newData)
+		{
+			WillChangeValue("ReaderDataArray");
+			_data.Add(newData);
+			DidChangeValue("ReaderDataArray");
+		}
+
+	}
+}
