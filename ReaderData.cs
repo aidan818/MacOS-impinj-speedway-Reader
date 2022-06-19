c20c4b64053efc206231e07a276f0c8ba272e058
aidan818 <107783500+aidan818@users.noreply.github.com>
2022-06-18 23:56:55 -0500

    Add files via upload

---
+++ ReaderData.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Foundation;
+using AppKit;
+
+namespace impinjspeedwayreader
+{
+	[Register("ReaderData")]
+	public class ReaderData : NSObject
+	{
+		private string _EPC = "";
+		private string _lastTimeSeen = "";
+		private NSMutableArray _data = new NSMutableArray();
+
+		[Export("EPC")]
+		public string EPC
+        {
+			get { return _EPC; }
+			set {
+				WillChangeValue("EPC");
+				_EPC = value;
+				DidChangeValue("EPC");
+            }
+        }
+
+		[Export("LastTimeSeen")]
+		public string LastTimeSeen
+        {
+            get { return _lastTimeSeen; }
+			set
+            {
+				WillChangeValue("LastTimeSeen");
+				_lastTimeSeen = value;
+				DidChangeValue("LastTimeSeen");
+            }
+        }
+
+		[Export("ReaderDataArray")]
+		public NSArray Data
+        {
+			get { return _data; }
+        }
+
+		public ReaderData()
+        {
+
+        }
+
+		public ReaderData(string EPC, string lastTimeSeen)
+        {
+			this.EPC = EPC;
+			this.LastTimeSeen = lastTimeSeen;
+        }
+
+		[Export("addObject:")]
+		public void AddData(ReaderData newData)
+        {
+			WillChangeValue("ReaderDataArray");
+			_data.Add(newData);
+			Console.WriteLine(_data);
+			DidChangeValue("ReaderDataArray");
+        }
+
+		
+	}
+}
+
---
+++ ViewController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Threading;
+
+using AppKit;
+using Foundation;
+
+using Impinj.OctaneSdk;
+
+
+namespace impinjspeedwayreader
+{
+	public partial class ViewController : NSViewController
+	{
+		public ViewController(IntPtr handle) : base(handle)
+		{
+		}
+
+        public ViewController()
+        {
+        }
+
+        public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+
+			UpdateData(GlobalVariable.EPC, GlobalVariable.LastTimeSeen);
+		}
+
+		public void UpdateData(string EPC, string LastTimeSeen)
+        {
+			AddData(new ReaderData(EPC, LastTimeSeen));
+			ReaderTable.ReloadData();
+		}
+		
+
+		public override NSObject RepresentedObject {
+			get {
+				return base.RepresentedObject;
+			}
+			set {
+				base.RepresentedObject = value;
+				// Update the view, if already loaded.
+			}
+		}
+
+
+		partial void StartButtonAction(NSButton sender)
+        {
+			NewSettings.CustomSettings();
+			
+			//Console.WriteLine("EPC: {0}. Timestamp: {1}", GlobalVariable.EPC, GlobalVariable.LastTimeSeen);
+			//Call a method/function that will start taking the data and providing it to the table
+		}
+		partial void StopButtonAction(NSButton sender)
+        {
+			GlobalVariable.reader.Stop();
+			Console.WriteLine("Reader Stop");
+        }
+
+        partial void SubmitIPAction(NSButton sender)
+        {
+			GlobalVariable.Host_IP = EnterIPOutlet.StringValue;
+			GlobalVariable.reader.Name = EnterNameOutlet.StringValue;
+			Console.WriteLine("The reader is now named: " + GlobalVariable.reader.Name);
+			Console.WriteLine("Now attempting to connect to: " + GlobalVariable.Host_IP);
+			Connection.ConnectToReader();
+			//Call function to predetermine the settings if reader was connected successfully
+        }
+
+		
+		private NSMutableArray _data = new NSMutableArray();
+
+		[Export("ReaderDataArray")]
+		public NSArray Data
+		{
+			get { return _data; }
+		}
+
+		[Export("addObject:")]
+		public void AddData(ReaderData newData)
+		{
+			WillChangeValue("ReaderDataArray");
+			_data.Add(newData);
+			DidChangeValue("ReaderDataArray");
+		}
+
+	}
+}
